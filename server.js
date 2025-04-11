e508e61fa893e2be7d9e4cdf4c51fbd10e72e5c1
Girish7549 <girish@adzdrio.com>
2025-04-11 11:00:42 +0530

    11/4/25 11:00

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 app.use(morgan('common'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:4000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:4000', 'https://adzdrio-exam-admin-panel.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
